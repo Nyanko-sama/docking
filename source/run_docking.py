de7c7ae1e20f464e58bc414b11b3fac1a100e0a1
Samo <samofanci@gmail.com>
2025-11-14 13:53:24 +0100

    almost doable, need optimization for batch molecule scrubbing

--- source/run_docking.py
+++ source/run_docking.py
@@ -43,11 +43,12 @@ else:
     raise ValueError('Unsupported OS.')
 
 scrub = locate_file(from_path = Path.cwd().parent, query_path = "scrub.py", query_name = "scrub.py")
+vina = locate_file(from_path=Path.cwd().parent, query_path=f'vina*', query_name='AutoDock Vina')
 mk_prepare_ligand = locate_file(from_path = path_root, query_path = "mk_prepare_ligand.py", query_name = "mk_prepare_ligand.py")
 mk_prepare_receptor = locate_file(from_path = path_root, query_path = "mk_prepare_receptor.py", query_name = "mk_prepare_receptor.py")
 mk_export = locate_file(from_path = path_root, query_path = "mk_export.py", query_name = "mk_export.py")
 
-def prepare_ligand(ligand_smiles, ph = 6, skip_tautomer=False, skip_acidbase=False, ligand_name='test_ligand'):
+def prepare_ligand(ligand_smiles, ph = 6, skip_tautomer=False, skip_acidbase=False, ligand_name='test_ligand') -> Path:
     # Adapted from https://colab.research.google.com/drive/1cHSl78lBPUc_J1IZxLgN4GwD_ADmohVU?usp=sharing#scrollTo=qBQN6WzwvkGB
     args = ""
     if skip_tautomer:
@@ -58,14 +59,33 @@ def prepare_ligand(ligand_smiles, ph = 6, skip_tautomer=False, skip_acidbase=Fal
 
     ligand_name = re.sub(r'-', '_', ligand_name)
     ligandSDF = f"{ligand_name}_scrubbed.sdf"
+    output_path = f'../data/prepared_ligands/{ligand_name}.pdbqt'
     # Scrub the molecule
-
-    os.system(f'python {scrub} "{ligand_smiles}" -o {ligandSDF} --ph {ph} {args}')
+    subprocess.run([
+        'python', str(scrub), ligand_smiles, '-o', ligandSDF, '--ph', str(ph) + args
+    ], check=True)
+    #os.system(f'python {scrub} "{ligand_smiles}" -o {ligandSDF} --ph {ph} {args}')
 
     # Runs meeko mk_prepare_ligand with the following arguments
-    os.system(f'python {mk_prepare_ligand} -i ../data/{ligandSDF} -o ../data/{ligand_name}.pdbqt')
+    subprocess.run([
+        'python', str(mk_prepare_ligand), '-i', f'../data/{ligandSDF}', '-o', output_path
+    ], check=True)
 
-def prepare_receptor(pdb_path : Path, center_coords, box_sizes):
+    return Path(output_path)
+
+
+def prepare_ligands(args) -> list[Path]:
+    ligands = pd.read_csv(args.ligands_path, sep='\t')
+    if not os.path.exists('../data/prepared_ligands'):
+        os.makedirs('../data/prepared_ligands')
+    lig_paths = []
+    
+    for name, smiles in zip(ligands['name'], ligands['smiles']):
+        lig_paths.append(prepare_ligand(smiles, ligand_name=name))
+
+    return lig_paths
+
+def prepare_receptor(pdb_path : Path, center_coords, box_sizes) -> list[Path]:
     # Export receptor atoms
     center_x, center_y, center_z = center_coords
     size_x, size_y, size_z = box_sizes
@@ -83,34 +103,56 @@ def prepare_receptor(pdb_path : Path, center_coords, box_sizes):
 
     subprocess.run(command, check=True)
 
-def dock_ligands(args):
-    if not os.path.exists('../data/docking_files'):
-        os.makedirs('../data/docking_files')
+    return (Path(f'../data/docking_files/{pdb_path.stem}_prepared.pdbqt'), Path(f'../data/docking_files/{pdb_path.stem}_prepared.box.txt'))
 
-    ligands = pd.read_csv(args.ligands_path, sep='\t')
+def prepare_receptors(args) -> list[tuple[Path, Path]]:
     pdbs = list(Path(args.pdbs_path).rglob('*.pdb'))
-    # for name, smiles in zip(ligands['name'], ligands['smiles']):
-    #     prepare_ligand(smiles, ligand_name=name)
-
+    out = []
     for pdb in pdbs:
-        name = pdb.name
+        name = pdb.stem
         try:
-            pockets = pd.read_csv(f'../p2rank_output/{name}.pdb_predictions.csv')
-        except FileExistsError:
+            pockets = pd.read_csv(f'{args.pocket_preds_path}/{name}.pdb_predictions.csv', skipinitialspace=True)
+        except FileNotFoundError:
             raise FileExistsError(f'Predictions for {name} not found. Use run_p2rank.py to generate the pocket predictions first.')
         
         best = pockets.sort_values('score').iloc[0]
-        prepare_receptor(pdb, (best['center_x'], best['center_y'], best['center_z']), (args.box_size, args.box_size, args.box_size))
+        out.append(prepare_receptor(pdb, (best['center_x'], best['center_y'], best['center_z']), (args.box_size, args.box_size, args.box_size)))
+    
+    return out
+
+def dock_ligands(receptor_info : list[tuple[Path, Path]], lig_paths : list[Path]):
+    if not os.path.exists('../data/docking_files'):
+        os.makedirs('../data/docking_files')
+
+    if not os.path.exists('../output'):
+        os.mkdir('../output')
+
+    for receptor, config in receptor_info:
+        for ligand in lig_paths:
+            subprocess.run([
+                str(vina),
+                '--receptor', str(receptor),
+                '--ligand', str(ligand),
+                '--config', str(config),
+                f'--exhaustiveness={args.e}',
+                '--out', f'../output/{receptor.stem}_{ligand.stem}'
+            ])
+
+def run_docking(args):
+    lig_paths = prepare_ligands(args)
+    receptor_info = prepare_receptors(args)
+    dock_ligands(receptor_info, lig_paths)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output_path', default='../data/p2rank_output', help='Output directory path')
+    parser.add_argument('--pocket_preds_path', default='../data/p2rank_output', help='Output directory path')
     parser.add_argument('--pdbs_path', default='../data/pdbs', help='Path to the directory where .pdb files are stored')
     parser.add_argument('--ligands_path', default='../data/ligands.csv', help='Path to ligands')
     parser.add_argument('--ph', default=6, type=int, help='pH for ligand preparation')
     parser.add_argument('--skip_tautomer', action='store_true', help='Skip tautomers in ligand preparation')
     parser.add_argument('--skip_acidbase', action='store_true', help='Skip acidbase in ligand preparation')
     parser.add_argument('--box_size', type=int, default=40, help='Box size in angstroms for docking. Default is the Prankweb value.')
+    parser.add_argument('-e', '--exhaustiveness', type=int, default=32, help='Search exhaustiveness for Vina')
 
     args = parser.parse_args()
-    dock_ligands(args)
+    run_docking(args)
