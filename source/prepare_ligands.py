6443292869214b00563efa58709c78b833b20e66
Samo <samofanci@gmail.com>
2025-11-25 18:12:21 +0100

    separated ligand preparation

---
+++ source/prepare_ligands.py
@@ -0,0 +1,97 @@
+import argparse
+import re
+import sys
+import os
+import subprocess
+import pandas as pd
+from pathlib import Path
+from utils import locate_file
+
+if sys.platform == 'linux':
+    path_root = Path("/usr/local/bin")
+elif sys.platform == 'win32':
+    path_root = Path(sys.executable).parent
+else:
+    raise ValueError('Unsupported OS.')
+
+mk_prepare_ligand = locate_file(from_path = path_root, query_path = "mk_prepare_ligand.py", query_name = "mk_prepare_ligand.py")
+scrub = locate_file(from_path = Path.cwd().parent, query_path = "scrub.py", query_name = "scrub.py")
+
+def prepare_ligand(ligand_smiles, ph = 6, skip_tautomer=False, skip_acidbase=False, ligand_name='test_ligand') -> Path:
+    # Adapted from https://colab.research.google.com/drive/1cHSl78lBPUc_J1IZxLgN4GwD_ADmohVU?usp=sharing#scrollTo=qBQN6WzwvkGB
+    args = ""
+    if skip_tautomer:
+        args += "--skip_tautomer "
+
+    if skip_acidbase:
+        args += "--skip_acidbase "
+
+    ligand_name = re.sub(r'-', '_', ligand_name)
+    ligandSDF = f"../data/temp/{ligand_name}_scrubbed.sdf"
+    output_path = f'../data/prepared_ligands/{ligand_name}.pdbqt'
+    # Scrub the molecule
+    subprocess.run([
+        'python', str(scrub), ligand_smiles, '-o', ligandSDF, '--ph', str(ph) + args
+    ], check=True)
+
+    # Runs meeko mk_prepare_ligand with the following arguments
+    subprocess.run([
+        'python', str(mk_prepare_ligand), '-i', f'../data/{ligandSDF}', '-o', output_path
+    ], check=True)
+
+    return Path(output_path)
+
+
+def standardize_name(ligand_name):
+    ligand_name = re.sub(r'\'"', '', ligand_name)
+    return re.sub(r'[-, ]', '_', ligand_name)
+
+def prepare_ligands(args):
+    ligands = pd.read_csv(args.ligands_path, sep='\t')
+    ligands = ligands.reindex(['smiles', 'name'], axis=1)
+    ligands['name'] = ligands['name'].apply(standardize_name)
+
+    batch = False
+    if ligands.shape[0] > 1:
+        batch = True
+
+    if not os.path.exists('../data/prepared_ligands'):
+        os.makedirs('../data/prepared_ligands')
+
+    if not os.path.exists('../data/temp'):
+        os.makedirs('../data/temp')
+    
+    if batch:
+        smi_path = '../data/temp/ligands.smi'
+        sdf_path = '../data/temp/ligand_batch.sdf'
+        ligands.to_csv(smi_path, sep=' ', header=False, index=False)
+        output_path = f'../data/prepared_ligands/'
+        scrub_additional_args = ''
+        if args.skip_tautomer:
+            scrub_additional_args += '--skip_tautomer '
+        if args.skip_acidbase:
+            scrub_additional_args += '--skip_acidbase '
+
+        # Scrub the molecules
+        subprocess.run([
+            'python', str(scrub), smi_path, '-o', sdf_path, '--ph', str(args.ph) + scrub_additional_args
+        ], check=True, shell=True)
+
+        subprocess.run([
+            'python', str(mk_prepare_ligand), '-i', sdf_path, '--multimol_outdir', output_path
+        ], check=True, shell=True)
+        
+    else:
+        prepare_ligand(ligands['smiles'], ligand_name=ligands['name'])
+
+    return ligands['name']
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ligands_path', default='../data/ligands.csv', help='Path to the ligands csv file. Should contain fields "smiles" and "name".')
+    parser.add_argument('--ph', default=7, type=int, help='pH for ligand preparation')
+    parser.add_argument('--tol', default=20, type=int, help='Maximum pocket center distance from the centroid of residues matched to the MSA indices for membrane pocket selection. Pockets further away are not considered when determining the best pocket.')
+    parser.add_argument('--skip_acidbase', action='store_true', help='Skip acid/base conjugates in ligand preparation')
+
+    args = parser.parse_args()
+    prepare_ligands(args)
