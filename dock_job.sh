41c98d201a0d5c24605ad9950d70e1873606f3fb
Nyanko-sama <neliakorotushak@gmail.com>
2025-12-07 18:32:29 +0100

    jjj

---
+++ dock_job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#PBS -N docking
+# Alfrid lebo ztadial sa mi dal spustit Mamba(conda) environment, z nejakoho dovodu to nefungovalo na elmo clusteri
+#PBS -l select=1:ncpus=2:mem=4gb:scratch_local=10gb:cluster=alfrid
+#PBS -l walltime=6:00:00
+#PBS -m ae
+
+# Path to home directory
+HOMEDIR=/storage/praha1/home/nexuso1/
+
+# Path to singularity image we want to use
+#IMAGE=/cvmfs/singularity.metacentrum.cz/NGC/PyTorch\:24.10-py3.SIF
+
+# Probably not necessary
+export TMPDIR=$SCRATCHDIR
+export CACHEDIR=$SCRATCHDIR
+export LOCALCACHEDIR=$SCRATCHDIR
+
+cd $HOMEDIR
+cd prankdock
+module add mambaforge
+mamba env create -f environment.yaml
+mamba activate dock
+cd source
+echo "Running job with array index $PBS_ARRAY_INDEX"
+python run_docking.py -d ../data/splits_$PBS_ARRAY_INDEX
