1f09b1027301f4524b42b31817da6c01bbe8ce29
Nyanko-sama <neliakorotushak@gmail.com>
2025-12-09 12:46:08 +0100

    j

--- dock_job.sh
+++ dock_job.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 
 #PBS -N docking
-# Alfrid lebo ztadial sa mi dal spustit Mamba(conda) environment, z nejakoho dovodu to nefungovalo na elmo clusteri
-#PBS -l select=1:ncpus=2:mem=4gb:scratch_local=10gb:cluster=alfrid
+#PBS -l select=1:ncpus=2:mem=8gb:scratch_local=20gb:cluster=alfrid
 #PBS -l walltime=6:00:00
 #PBS -m ae
 
-# Path to home directory
-HOMEDIR=/storage/praha1/home/nexuso1/
+# Path to docking project root
+HOMEDIR=/storage/praha1/home/nelia_k/docking
 
-# Path to singularity image we want to use
-#IMAGE=/cvmfs/singularity.metacentrum.cz/NGC/PyTorch\:24.10-py3.SIF
-
-# Probably not necessary
 export TMPDIR=$SCRATCHDIR
 export CACHEDIR=$SCRATCHDIR
 export LOCALCACHEDIR=$SCRATCHDIR
 
-cd $HOMEDIR
-cd prankdock
+cd "$HOMEDIR/prankdock"
+
+# Load mambaforge and enable "mamba activate" in this non-interactive shell
 module add mambaforge
+eval "$(mamba shell hook -s bash)"
 mamba activate dock
+
+# Debug info to confirm env is correct (keep for now)
+echo "=== PYTHON INFO ==="
+which python
+python -c "import sys; print('python exe:', sys.executable)"
+python -c "import molscrub; print('molscrub module:', molscrub.__file__)"
+
 cd source
 echo "Running full pipeline with array index $PBS_ARRAY_INDEX"
-python run_p2rank.py
+
+# if needed:
+python run_p2rank.py --p2rank_path ../p2rank_2.5.1
+
 python prepare_ligands.py --timeout 240
 python prepare_receptors.py
 python run_docking.py -d ../data/splits_$PBS_ARRAY_INDEX
